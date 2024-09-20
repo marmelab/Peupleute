7207b157a523f520bea8b2ff44b9cddf2c232dd8
atilbian <arnaud@marmelab.com>
2024-09-20 14:51:31 +0200

    remove great tit as there are some corrupted files

--- dowload_bird_songs.py
+++ dowload_bird_songs.py
@@ -1,7 +1,7 @@
 from xenocanto import download
 import asyncio
 
-scientificNames = ["Parus major","Cyanistes caeruleus","Lophophanes cristatus","Turdus merula","Passer domesticus", "Pica pica","Streptopelia decaocto","Corvus corone","Columba palumbus","Sturnus vulgaris","Periparus ater","Dendrocopos major","Picus viridis","Passer montanus"]
+scientificNames = ["Cyanistes caeruleus","Lophophanes cristatus","Turdus merula","Passer domesticus", "Pica pica","Streptopelia decaocto","Corvus corone","Columba palumbus","Sturnus vulgaris","Periparus ater","Dendrocopos major","Picus viridis","Passer montanus"]
 
 for name in scientificNames:
     asyncio.run(download([name, 'q:A', 'cnt:France', 'len:5-20']))
