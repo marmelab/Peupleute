f67822df4586a7ab4f5d3a81c8802d4a09fcf42e
atilbian <arnaud@marmelab.com>
2024-09-20 16:02:49 +0200

    add endpoint to calc file size

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/file-size/")
+async def get_file_size(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
